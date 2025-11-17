60cf4d096a441835c68f97325a2f6a871299c41e
AliJafarli <somagni4icent@gmail.com>
2025-11-17 17:14:01 +0400

    liquibase added

--- src/main/resources/db.migration/V1__create_users_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       username VARCHAR(200) NOT NULL,
-                       email VARCHAR(200) UNIQUE NOT NULL,
-                       role VARCHAR(50) NOT NULL DEFAULT 'USER',
-                       created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-                       updated_at TIMESTAMP WITH TIME ZONE NOT NULL
-);
